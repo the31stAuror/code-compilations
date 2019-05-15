69571524db65bd89195684ca2a3c6d98cff5a1c4
jeadavid <jeanne.david@nokia.com>
2019-05-15 14:59:26 +0800

    added more sample codes

--- javascript/raw-js.js
+++ javascript/raw-js.js
@@ -80,12 +80,88 @@ let enrollment = [
 // console.log(average(grades));
 
 // functional chain
-const _ = require('lodash');
-const chained = _.chain(enrollment)
-    .filter(student => student.enrolled > 1)
-    .map(student => student.grade)
-    .value();
-console.log(chained);
+// const _ = require('lodash');
+// const chained = _.chain(enrollment)
+//     .filter(student => student.enrolled > 1)
+//     .map(student => student.grade)
+//     .value();
+// console.log(chained);
+
+class Person {
+  constructor(firstname, lastname, ssn) {
+    this._firstname = firstname;
+    this._lastname = lastname;
+    this._ssn = ssn;
+    this._address = null;
+    this._birthYear = null;
+  }
+
+  get ssn() {
+    return this._ssn;
+  }
+
+  get firstname() {
+    return this._firstname;
+  }
+
+  get lastname() {
+    return this._lastname;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get birthYear() {
+    return this._birthYear;
+  }
+
+  set birthYear(year) {
+    this._birthYear = year;
+  }
+
+  set address(address) {
+    this._address = address;
+  }
+
+  toString() {
+    return `Person(${this._firstname}, ${this._lastname}`;
+  }
+
+  peopleInSameCountry(friends) {
+    let result = [];
+    for (let idx in friends) {
+      let friend = friends [idx];
+      if (this.address.country === friend.address.country) {
+        result.push(friend);
+      }
+    }
+    return result;
+  }
+}
+
+class Student extends Person {
+  constructor(firstname, lastname, ssn, school) {
+    super(firstname, lastname, ssn);
+    this._school = school;
+  }
+
+  get school() {
+    return this._school;
+  }
+
+  studentsInSameCountryAndSchool(friends) {
+    let closeFriends = super.peopleInSameCountry(friends);
+    let result = [];
+    for (let idx in closeFriends) {
+      let friend = closeFriends[idx];
+      if (friend.school === this.school) {
+        result.push(friend);
+      }
+    }
+    return result;
+  }
+}
 
 // let starks = [
 //     { "name": "Ned", "status": "dead" },
